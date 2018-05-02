d8f729cf7967cec330db5b4ec1f37bb38c76b690
jsondoo <jsondoo@users.noreply.github.com>
2018-05-02 14:14:02 -0700

    Add different courses

--- index.js
+++ index.js
@@ -48,20 +48,22 @@ const writeData = async (arr) => {
   })
 }
 
-const writeCourseData = () => {
-  const numEvals = randomIntFromInterval(1,60) // number of evaluations
-  const surveyname = "LFS Instructor/Course Evaluation 2017SA"
+const writeCourseData = (surveyyear) => {
+  const numEvals = randomIntFromInterval(1,100) // number of evaluations
+  const course = getRandomCourse()
+
+  const surveyname = "LFS Instructor/Course Evaluation " + surveyyear
   const datestart = ""
   const dateclose = ""
-  const crsnum = "APBI 100 98A"
-  const crsname = "Soil and Global Environment"
-  const crsyear = "1"
-  const deptname = "APBI"
-  const crs_dir = "INSTRUCTOR NAME"
-  const resp_fac = "INSTRUCTOR NAME"
+  const crsnum = course.crsnum
+  const crsname = course.crsname
+  const crsyear = course.crsyear
+  const deptname = course.deptname
+  const crs_dir = course.crs_dir
+  const resp_fac = course.resp_fac
   const eval_id = ""
-  const eval_uname = "WEUIF89MZX9F9"
-  const eval_email = "john.smith@ubc.ca"
+  const eval_uname = course.eval_uname
+  const eval_email = course.eval_email
   const tsubmit = ""
   const gradyear = ""
   const research1 = ""
@@ -105,12 +107,113 @@ function randomIntFromInterval(min,max) {
   return Math.floor(Math.random()*(max-min+1)+min);
 }
 
+function getRandomCourse() {
+  const courses = [
+    {
+      crsnum: "LFS 100 001",
+      crsname: "Introduction to Land, Food and Community",
+      crsyear: "1",
+      deptname: "LFS",
+      crs_dir: "John Smith",
+      resp_fac: "John Smith",
+      eval_uname: "MXUE8WMQMX9",
+      eval_email: "john.smith@ubc.ca"
+    },
+    {
+      crsnum: "LFS 250 001",
+      crsname: "Land, Food and Community I",
+      crsyear: "2",
+      deptname: "LFS",
+      crs_dir: "Tony Stark",
+      resp_fac: "Tony Stark",
+      eval_uname: "LKS4D9JFKL2",
+      eval_email: "tony.stark@ubc.ca"
+    },
+    {
+      crsnum: "HUNU 505 001",
+      crsname: "Current Issues in Applied Nutrition",
+      crsyear: "5",
+      deptname: "HUNU",
+      crs_dir: "William Lee",
+      resp_fac: "William Lee",
+      eval_uname: "KJL238X9Z",
+      eval_email: "william.lee@ubc.ca"
+    },
+    {
+      crsnum: "FRE 295 002",
+      crsname: "Managerial Economics",
+      crsyear: "2",
+      deptname: "FRE",
+      crs_dir: "Ed Parker",
+      resp_fac: "Ed Parker",
+      eval_uname: "MZ98POPLQZ9",
+      eval_email: "ed.parker@ubc.ca"
+    },
+    {
+      crsnum: "FRE 302 001",
+      crsname: "Small Business Management in Agri-food Industries",
+      crsyear: "3",
+      deptname: "FRE",
+      crs_dir: "Jennifer Berg",
+      resp_fac: "Jennifer Berg",
+      eval_uname: "A0OZP2L1L",
+      eval_email: "jennifer.berg@ubc.ca"
+    },
+    {
+      crsnum: "FOOD 500 001",
+      crsname: "Small Business Management in Agri-food Industries",
+      crsyear: "3",
+      deptname: "FRE",
+      crs_dir: "Jennifer Berg",
+      resp_fac: "Jennifer Berg",
+      eval_uname: "A0OZP2L1L",
+      eval_email: "jennifer.berg@ubc.ca"
+    },
+    {
+      crsnum: "GRS 390 001",
+      crsname: "Global Issues in Cultural Context",
+      crsyear: "3",
+      deptname: "GRS",
+      crs_dir: "Anne Lo",
+      resp_fac: "Anne Lo",
+      eval_uname: "GOO8X0X9",
+      eval_email: "anne.lo@ubc.ca"
+    },
+    {
+      crsnum: "FNH 200 001",
+      crsname: "Exploring Our Food",
+      crsyear: "2",
+      deptname: "FNH",
+      crs_dir: "Timmy Anderson",
+      resp_fac: "Timmy Anderson",
+      eval_uname: "DPQ91MZ62J",
+      eval_email: "timmy.anderson@ubc.ca"
+    },
+    {
+      crsnum: "FNH 200 102",
+      crsname: "Exploring Our Food",
+      crsyear: "2",
+      deptname: "FNH",
+      crs_dir: "Timmy Anderson",
+      resp_fac: "Timmy Anderson",
+      eval_uname: "KMMCOX1092F",
+      eval_email: "timmy.anderson@ubc.ca"
+    }
+  ]
+
+  const index = Math.floor(Math.random() * courses.length)
+  return courses[index]
+}
+
 const generateCSV = () => {
   writeHeader()
-  writeCourseData()
-  writeCourseData()
-  writeCourseData()
-  writeCourseData()
+  writeCourseData("2017W1")
+  writeCourseData("2016W2")
+  writeCourseData("2016W1")
+  writeCourseData("2015W2")
+  writeCourseData("2015W2")
+  writeCourseData("2015W1")
+  writeCourseData("2015W1")
 }
 
 generateCSV()
