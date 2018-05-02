fbfe5712089d2345b7eed1f4c4c7af349df96645
jsondoo <jsondoo@users.noreply.github.com>
2018-05-02 13:29:03 -0700

    Generate data for a single course

--- index.js
+++ index.js
@@ -49,7 +49,7 @@ const writeData = async (arr) => {
 }
 
 const writeCourseData = () => {
-  const numEvals = 5 // number of evaluations
+  const numEvals = randomIntFromInterval(1,60) // number of evaluations
   const surveyname = "LFS Instructor/Course Evaluation 2017SA"
   const datestart = ""
   const dateclose = ""
@@ -63,32 +63,53 @@ const writeCourseData = () => {
   const eval_uname = "WEUIF89MZX9F9"
   const eval_email = "john.smith@ubc.ca"
   const tsubmit = ""
-  let mobile = ""         // empty or 0 or 1
   const gradyear = ""
-  let gender = "Male"     // male or female
   const research1 = ""
   const research2 = ""
   const research3 = ""
-
-  // random score from [1,5]
-  let UMI1 = "1"           
-  let UMI2 = "2"
-  let UMI3 = "3"
-  let UMI4 = "4"
-  let UMI5 = "5"
-  let UMI6 = "1"
+  
+  // variables
+  let mobile
+  let gender  
+  let UMI1            
+  let UMI2 
+  let UMI3 
+  let UMI4 
+  let UMI5 
+  let UMI6
 
   for(let i = 0; i < numEvals; ++i) {
     let arr = []
+
+    mobile = randomIntFromInterval(0,1)                              // 0 or 1
+    gender = randomIntFromInterval(0,1) == 0 ? "Male" : "Female"     // male or female
+
+    // random score from [1,5]
+    UMI1 = randomIntFromInterval(1,5)           
+    UMI2 = randomIntFromInterval(1,5)
+    UMI3 = randomIntFromInterval(1,5)
+    UMI4 = randomIntFromInterval(1,5)
+    UMI5 = randomIntFromInterval(1,5)
+    UMI6 = randomIntFromInterval(1,5)
+
     arr = [surveyname, datestart, dateclose, crsnum, crsname, crsyear, deptname, 
       crs_dir, resp_fac, eval_id, eval_uname, eval_email, tsubmit, mobile, gradyear, gender,
       research1, research2, research3, UMI1, UMI2, UMI3, UMI4, UMI5, UMI6]
+
     writeData(arr)
   }
 }
 
+// returns a random integer from [min,max]
+function randomIntFromInterval(min,max) {
+  return Math.floor(Math.random()*(max-min+1)+min);
+}
+
 const generateCSV = () => {
   writeHeader()
+  writeCourseData()
+  writeCourseData()
+  writeCourseData()
   writeCourseData()
 }
 
